049123087028f04c3dec23501737f49fc3f6a417
BrandonStookey <bwstookey@gmail.com>
2016-02-22 15:45:23 -0600

    (feat) Implemented Chat

--- client/v.tasks/tasks.js
+++ client/v.tasks/tasks.js
@@ -4,38 +4,3 @@ angular.module('collaby.tasks', [])
 
 
 });
-
-
-/* Example from Shortly-angular. This is the controller for the filterable list of links:
-
-angular.module('shortly.links', [])
-
-.controller('LinksController', function ($scope, Links) {
-  $scope.data = {
-    links: []
-  };
-  $scope.getLinks = function(){
-    Links.getLinks($scope.data.links);
-  };
-  $scope.getLinks();
-})
-.factory('Links', function Links ($http) {
-  var data = {};
-  var getLinks = function() {
-    return $http({
-      method: 'GET',
-      url: '/api/links'
-    })
-    .then(function (result) {
-      data.links = result.data;
-      return data.links;
-    });
-  };
-
-  return {
-    data: data,
-    getLinks: getLinks
-  };
-});
-
-*/
