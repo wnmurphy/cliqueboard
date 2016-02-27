195b65e62a512460d69e07caaa5b89063ef20f72
BrandonStookey <bwstookey@gmail.com>
2016-02-26 18:12:30 -0600

    j

--- client/main/main.js
+++ client/main/main.js
@@ -50,7 +50,7 @@ angular.module('twork.main', [])
   $scope.init();
   
   //Set up socket connection for incoming draw events
-  $scope.socket = io.connect('process.env.MONGOLAB_URI');
+  $scope.socket = io.connect('triceratest.heroku.com');
 
   // Create draw event listener which triggers local draw event.
   $scope.socket.on('draw', function(data){
@@ -90,7 +90,8 @@ angular.module('twork.main', [])
 .controller('chatController', function ($scope, $rootScope) {
   var userInfo = $rootScope.userData;
 
-  var socket = io.connect(window.location.hostname);
+  var socket = io.connect("triceratest.heroku.com");
+  // var socket = io.connect(window.location.hostname);
   // $scope.socket = io.connect('http://localhost:4568');
 
  // Store username of the currently logged-in user
