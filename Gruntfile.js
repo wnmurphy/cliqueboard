685244a04b78b989d01a4558faef5f45b340874f
BrandonStookey <bwstookey@gmail.com>
2016-02-28 12:41:48 -0600

    Add client/main to concat and  uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function(grunt) {
       },
       js: {
         // src: ['client/lib/jquery.js', 'client/lib/underscore.js','client/lib/backbone.js','client/lib/handlebars.js', 'client/client/*.js'],
-        src: ['client/lib/jquery.event.drag-2.0.js', 'client/login/*.js', 'client/app.js','client/server/*.js', 'client/test/*.js'],
+        src: ['client/lib/jquery.event.drag-2.0.js', 'client/login/*.js', 'client/app.js','client/server/*.js', 'client/test/*.js', 'client/main/*.js'],
         dest: 'client/dist/built.js'
       }
     },
